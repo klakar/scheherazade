4332d73ebe781714b1868bc96b3c432f19481752
[klakar] <[klaskarlsson@hotmail.com]>
2016-07-25 00:01:04 +0200

    More GUI Functionality

--- story/i18n/story_sv.ts
+++ story/i18n/story_sv.ts
@@ -11,142 +11,172 @@
 <context>
     <name>story</name>
     <message>
-        <location filename="story.py" line="202"/>
+        <location filename="story.py" line="210"/>
         <source>&amp;Story</source>
-        <translation type="unfinished">&amp;Story</translation>
+        <translation type="unfinished">&amp;Berättelse</translation>
     </message>
     <message>
-        <location filename="story.py" line="171"/>
+        <location filename="story.py" line="179"/>
         <source>Story</source>
-        <translation type="unfinished">Story</translation>
+        <translation type="unfinished">Berättelse</translation>
     </message>
     <message>
-        <location filename="story.py" line="213"/>
-        <source>test</source>
-        <translation type="unfinished">test</translation>
+        <location filename="story.py" line="368"/>
+        <source>Slide Title</source>
+        <translation type="unfinished">Bildrubrik</translation>
+    </message>
+    <message>
+        <location filename="story.py" line="369"/>
+        <source>Content</source>
+        <translation type="unfinished">Innehåll</translation>
+    </message>
+    <message>
+        <location filename="story.py" line="285"/>
+        <source>Message!</source>
+        <translation type="unfinished">Meddelande!</translation>
+    </message>
+    <message>
+        <location filename="story.py" line="262"/>
+        <source>This is a test...</source>
+        <translation type="unfinished">Detta är ett test...</translation>
+    </message>
+    <message>
+        <location filename="story.py" line="308"/>
+        <source>Slide %s/%s</source>
+        <translation type="unfinished">Bild %s/%s</translation>
+    </message>
+    <message>
+        <location filename="story.py" line="356"/>
+        <source>The main Story Title</source>
+        <translation type="unfinished">Berättelsens Huvudtitel</translation>
     </message>
 </context>
 <context>
     <name>storyDockWidgetBase</name>
     <message>
-        <location filename="story_dockwidget_base.ui" line="20"/>
-        <source>Story Teller</source>
-        <translation type="unfinished">Berättelse</translation>
-    </message>
-    <message>
-        <location filename="story_dockwidget_base.ui" line="36"/>
-        <source>Story Properties</source>
-        <translation type="unfinished">Story Egenskaper</translation>
-    </message>
-    <message>
-        <location filename="story_dockwidget_base.ui" line="636"/>
+        <location filename="story_dockwidget_base.ui" line="650"/>
         <source>Title</source>
         <translation type="unfinished">Titel</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="170"/>
+        <location filename="story_dockwidget_base.ui" line="177"/>
         <source>Style</source>
         <translation type="unfinished">Stil</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="282"/>
+        <location filename="story_dockwidget_base.ui" line="289"/>
         <source>Style 1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="287"/>
+        <location filename="story_dockwidget_base.ui" line="294"/>
         <source>Style 2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="292"/>
+        <location filename="story_dockwidget_base.ui" line="299"/>
         <source>Style 3</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="304"/>
+        <location filename="story_dockwidget_base.ui" line="311"/>
         <source>Base Map</source>
         <translation type="unfinished">Bakgrundskarta</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="389"/>
+        <location filename="story_dockwidget_base.ui" line="396"/>
         <source>Dummy Map 1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="394"/>
+        <location filename="story_dockwidget_base.ui" line="401"/>
         <source>Dummy Map 2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="415"/>
-        <source>File</source>
-        <translation type="unfinished">Fil</translation>
-    </message>
-    <message>
-        <location filename="story_dockwidget_base.ui" line="424"/>
+        <location filename="story_dockwidget_base.ui" line="431"/>
         <source>Story File</source>
-        <translation type="unfinished">Story Fil</translation>
+        <translation type="unfinished">Berrättelse Fil</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="518"/>
+        <location filename="story_dockwidget_base.ui" line="525"/>
         <source>...</source>
         <translation type="unfinished">...</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="542"/>
+        <location filename="story_dockwidget_base.ui" line="556"/>
         <source>Create/Publish</source>
         <translation type="unfinished">Skapa/Publicera</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="575"/>
-        <source>Slide</source>
-        <translation type="unfinished">Bild</translation>
-    </message>
-    <message>
-        <location filename="story_dockwidget_base.ui" line="584"/>
+        <location filename="story_dockwidget_base.ui" line="598"/>
         <source>Previous</source>
         <translation type="unfinished">Föregående</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="591"/>
+        <location filename="story_dockwidget_base.ui" line="605"/>
         <source>Slide n/nn</source>
         <translation type="unfinished">Bild n/nn</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="598"/>
+        <location filename="story_dockwidget_base.ui" line="612"/>
         <source>Next</source>
         <translation type="unfinished">Nästa</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="618"/>
+        <location filename="story_dockwidget_base.ui" line="632"/>
         <source>Remove Slide</source>
         <translation type="unfinished">Ta bort Bild</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="625"/>
+        <location filename="story_dockwidget_base.ui" line="639"/>
         <source>Add Slide</source>
         <translation type="unfinished">Lägg till Bild</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="734"/>
+        <location filename="story_dockwidget_base.ui" line="748"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Content&lt;/p&gt;&lt;p&gt;(HTML)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Innehåll&lt;/p&gt;&lt;p&gt;(HTML)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="754"/>
+        <location filename="story_dockwidget_base.ui" line="768"/>
         <source>Zoom</source>
         <translation type="unfinished">Zoom</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="849"/>
+        <location filename="story_dockwidget_base.ui" line="863"/>
         <source>Position</source>
         <translation type="unfinished">Position</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="946"/>
+        <location filename="story_dockwidget_base.ui" line="960"/>
         <source>Get From Map</source>
         <translation type="unfinished">Hämta Från Karta</translation>
+    </message>
+    <message>
+        <location filename="story_dockwidget_base.ui" line="549"/>
+        <source>Save .story</source>
+        <translation type="unfinished">Spara .story</translation>
+    </message>
+    <message>
+        <location filename="story_dockwidget_base.ui" line="27"/>
+        <source>Story</source>
+        <translation type="unfinished">Berättelse</translation>
+    </message>
+    <message>
+        <location filename="story_dockwidget_base.ui" line="43"/>
+        <source>&lt;b&gt;Main&lt;/b&gt;</source>
+        <translation type="unfinished">&lt;b&gt;Generellt&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="story_dockwidget_base.ui" line="422"/>
+        <source>&lt;b&gt;File&lt;/b&gt;</source>
+        <translation type="unfinished">&lt;b&gt;Filer&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="story_dockwidget_base.ui" line="589"/>
+        <source>&lt;b&gt;Slides&lt;/b&gt;</source>
+        <translation type="unfinished">&lt;b&gt;Bilder&lt;/b&gt;</translation>
     </message>
 </context>
 </TS>
