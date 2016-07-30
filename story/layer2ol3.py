904195517e1a6a127151e4965a96fbeea7d43190
[klakar] <[klaskarlsson@hotmail.com]>
2016-07-30 13:59:44 +0200

    WMS support

--- story/layer2ol3.py
+++ story/layer2ol3.py
@@ -45,4 +45,22 @@ def iterate(publishPath):
 			#qgis.core.QgsVectorFileWriter.writeAsVectorFormat(canvasLayer, os.path.join(os.path.dirname(publishPath+"/"), '%s.kml' % canvasLayer.name()), 'utf-8', canvasLayer.crs(), 'KML') # Save layer to KML - Slow!!
 			canvasLayer.saveSldStyle(os.path.join(os.path.dirname(publishPath+"/"), '%s.sld' % canvasLayer.name()))
 			ol3layers += u",\n new ol.layer.Vector({\n title: '%s',\n source: new ol.source.Vector({\n format: new ol.format.GeoJSON(),\n url: '%s.json'\n })\n })" % (canvasLayer.name(), canvasLayer.name())
+		if canvasLayer.type() == 1:
+			# WMS layer
+			if canvasLayer.rasterType() == 3:
+				sourceString = canvasLayer.source()
+				sourceDict = dict(x.split('=') for x in sourceString.split('&'))
+				ol3layers += u'\n ,\n new ol.layer.Tile({\n  title: "%s",\n source: new ol.source.TileWMS({\n  url: "%s",\n  params: { layers: "%s" }\n })\n })' % (canvasLayer.name(), sourceDict[u'url'], sourceDict[u'layers'])
+				#QMessageBox.information(qgis.utils.iface.mainWindow(),(u"Message!"), sourceDict[u'url'])
 	return ol3layers
+
+
+
+
+
+
+
+
+
+
+
