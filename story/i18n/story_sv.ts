476feb7b249b99380a972754e5d2a218b75d9ae4
Klas Karlsson <klaskarlsson@hotmail.com>
2016-08-16 08:41:47 +0200

    Updated translation to SWE

--- story/i18n/story_sv.ts
+++ story/i18n/story_sv.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="sv_SE" sourcelanguage="en">
+<!DOCTYPE TS><TS version="2.0" language="sv_SE" sourcelanguage="en">
 <context>
     <name>@default</name>
     <message>
@@ -22,7 +21,7 @@
         <translation>Berättelse</translation>
     </message>
     <message>
-        <location filename="story.py" line="282"/>
+        <location filename="story.py" line="554"/>
         <source>Continue?</source>
         <translation>Fortsätta?</translation>
     </message>
@@ -32,12 +31,12 @@
         <translation>Ta bort aktuell bild?</translation>
     </message>
     <message>
-        <location filename="story.py" line="588"/>
+        <location filename="story.py" line="622"/>
         <source>Slide Title</source>
         <translation>Bildtitel</translation>
     </message>
     <message>
-        <location filename="story.py" line="589"/>
+        <location filename="story.py" line="623"/>
         <source>Content</source>
         <translation>Innehåll</translation>
     </message>
@@ -83,14 +82,24 @@ Kunde inte öppnas!
         <translation>Välja Katalog att spara Berättelsen i</translation>
     </message>
     <message>
-        <location filename="story.py" line="492"/>
+        <location filename="story.py" line="500"/>
         <source>Slide %s/%s</source>
         <translation>Bild %s/%s</translation>
     </message>
     <message>
-        <location filename="story.py" line="572"/>
+        <location filename="story.py" line="606"/>
         <source>The main Story Title</source>
         <translation>Berättelsens Titel</translation>
+    </message>
+    <message>
+        <location filename="story.py" line="471"/>
+        <source>Press OK to stop Story Server.</source>
+        <translation type="unfinished">Tryck OK för att stoppa Berättelsetjänsten.</translation>
+    </message>
+    <message>
+        <location filename="story.py" line="554"/>
+        <source>Reset Story Completely?</source>
+        <translation type="unfinished">Återställa Berättelsen Helt?</translation>
     </message>
 </context>
 <context>
@@ -101,247 +110,220 @@ Kunde inte öppnas!
         <translation>Berättelsepanel</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="81"/>
+        <location filename="story_dockwidget_base.ui" line="84"/>
         <source>Main Title and Template</source>
         <translation>Huvudtitel och Mallar</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="622"/>
+        <location filename="story_dockwidget_base.ui" line="661"/>
         <source>Title</source>
         <translation>Titel</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="195"/>
+        <location filename="story_dockwidget_base.ui" line="198"/>
         <source>Main Story Title</source>
         <translation>Huvudtitel</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="206"/>
+        <location filename="story_dockwidget_base.ui" line="209"/>
         <source>Template</source>
         <translation>Mall</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="277"/>
+        <location filename="story_dockwidget_base.ui" line="280"/>
         <source>Select Story Template</source>
         <translation>Välj Berättelsemall</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="288"/>
+        <location filename="story_dockwidget_base.ui" line="291"/>
         <source>Style</source>
         <translation>Stil</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="386"/>
+        <location filename="story_dockwidget_base.ui" line="389"/>
         <source>Select Story Style</source>
         <translation>Välj Berättelsestil</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="418"/>
+        <location filename="story_dockwidget_base.ui" line="459"/>
         <source>Slides</source>
         <translation>Bilder</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="428"/>
+        <location filename="story_dockwidget_base.ui" line="469"/>
         <source>Move to Previous Slide</source>
         <translation>Gå till föregående bild</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="446"/>
+        <location filename="story_dockwidget_base.ui" line="487"/>
         <source>Move Slide Earlier</source>
         <translation>Flytta bild tidigare</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="464"/>
+        <location filename="story_dockwidget_base.ui" line="505"/>
         <source>Slide n/nn</source>
         <translation>Bild n/nn</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="471"/>
+        <location filename="story_dockwidget_base.ui" line="512"/>
         <source>Move Slide Later</source>
         <translation>Flytta bild senare</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="486"/>
+        <location filename="story_dockwidget_base.ui" line="527"/>
         <source>Move to Next Slide</source>
         <translation>Gå till nästa bild</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="514"/>
+        <location filename="story_dockwidget_base.ui" line="555"/>
         <source>Option:</source>
         <translation>Alternativ:</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="522"/>
+        <location filename="story_dockwidget_base.ui" line="566"/>
         <source>Default</source>
         <translation>Standard</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="527"/>
-        <source>Option1</source>
-        <translation>Alternativ1</translation>
-    </message>
-    <message>
-        <location filename="story_dockwidget_base.ui" line="532"/>
-        <source>Option2</source>
-        <translation>Alternativ2</translation>
-    </message>
-    <message>
-        <location filename="story_dockwidget_base.ui" line="537"/>
-        <source>Option3</source>
-        <translation>Alternativ3</translation>
-    </message>
-    <message>
-        <location filename="story_dockwidget_base.ui" line="542"/>
-        <source>Option4</source>
-        <translation>Alternativ4</translation>
-    </message>
-    <message>
-        <location filename="story_dockwidget_base.ui" line="547"/>
-        <source>Option5</source>
-        <translation>Alternativ5</translation>
-    </message>
-    <message>
-        <location filename="story_dockwidget_base.ui" line="552"/>
-        <source>Option6</source>
-        <translation>Alternativ6</translation>
-    </message>
-    <message>
-        <location filename="story_dockwidget_base.ui" line="557"/>
-        <source>Option7</source>
-        <translation>Alternativ7</translation>
-    </message>
-    <message>
-        <location filename="story_dockwidget_base.ui" line="562"/>
-        <source>Option8</source>
-        <translation>Alternativ8</translation>
-    </message>
-    <message>
-        <location filename="story_dockwidget_base.ui" line="567"/>
-        <source>Option9</source>
-        <translation>Alternativ9</translation>
-    </message>
-    <message>
-        <location filename="story_dockwidget_base.ui" line="588"/>
+        <location filename="story_dockwidget_base.ui" line="627"/>
         <source>Remove Current Slide</source>
         <translation>Ta bort denna bild</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="603"/>
+        <location filename="story_dockwidget_base.ui" line="642"/>
         <source>Add Slide After Current</source>
         <translation>Lägg till efter denna</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="699"/>
+        <location filename="story_dockwidget_base.ui" line="738"/>
         <source>Slide Title</source>
         <translation>Bildtitel</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="710"/>
+        <location filename="story_dockwidget_base.ui" line="749"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Content&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Innehåll&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="723"/>
+        <location filename="story_dockwidget_base.ui" line="765"/>
         <source>Slide Content (HTML allowed)</source>
         <translation>Bildinnehåll (HTML tillåtet)</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="734"/>
+        <location filename="story_dockwidget_base.ui" line="779"/>
         <source>Position</source>
         <translation>Position</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="811"/>
+        <location filename="story_dockwidget_base.ui" line="856"/>
         <source>Longitude, Latitude</source>
         <translation>Longitud, Latitud</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="821"/>
+        <location filename="story_dockwidget_base.ui" line="866"/>
         <source>Zoom</source>
         <translation>Zoom</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="892"/>
+        <location filename="story_dockwidget_base.ui" line="937"/>
         <source>Zoom Level</source>
         <translation>Zoom nivå</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="902"/>
+        <location filename="story_dockwidget_base.ui" line="947"/>
         <source>Get Position and Zoom from Canvas</source>
         <translation>Hämta Position och Zoom från Kartan</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="929"/>
+        <location filename="story_dockwidget_base.ui" line="974"/>
         <source>Story File and Publish</source>
         <translation>Berättelsefil och Publicera</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="939"/>
+        <location filename="story_dockwidget_base.ui" line="984"/>
         <source>Story File</source>
         <translation>Berättelsefil</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="1016"/>
+        <location filename="story_dockwidget_base.ui" line="1061"/>
         <source>Current Story Save File</source>
         <translation>Nuvarande Berättelsefil</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="1023"/>
+        <location filename="story_dockwidget_base.ui" line="1068"/>
         <source>Clear Story Save Path</source>
         <translation>Rensa Sökväg</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="1055"/>
+        <location filename="story_dockwidget_base.ui" line="1100"/>
         <source>Open Story</source>
         <translation>Öppna Berättelse</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="1070"/>
+        <location filename="story_dockwidget_base.ui" line="1115"/>
         <source>Save Story</source>
         <translation>Spara Berättelse</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="1085"/>
+        <location filename="story_dockwidget_base.ui" line="1130"/>
         <source>Create/Publish Story</source>
         <translation>Skapa/Publicera Berättelse</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="1138"/>
+        <location filename="story_dockwidget_base.ui" line="1183"/>
         <source>Edit selected Story Style</source>
         <translation>Redigera vald Berättelsestil</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="1144"/>
+        <location filename="story_dockwidget_base.ui" line="1189"/>
         <source>No Style Selected</source>
         <translation>Ingen Stil Vald</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="1159"/>
+        <location filename="story_dockwidget_base.ui" line="1204"/>
         <source>About Story</source>
         <translation>Om Berättelse</translation>
     </message>
     <message>
-        <location filename="story_dockwidget_base.ui" line="1183"/>
+        <location filename="story_dockwidget_base.ui" line="407"/>
+        <source>Reset All</source>
+        <translation type="unfinished">Återställ Allt</translation>
+    </message>
+    <message>
+        <location filename="story_dockwidget_base.ui" line="427"/>
+        <source>Story Reset</source>
+        <translation type="unfinished">Återställ Berättelse</translation>
+    </message>
+    <message>
+        <location filename="story_dockwidget_base.ui" line="562"/>
+        <source>Style Option (if supported by css-file)</source>
+        <translation type="unfinished">Stilalternativ (om det stöds av css filen)</translation>
+    </message>
+    <message>
+        <location filename="story_dockwidget_base.ui" line="1228"/>
         <source>This is a testing version of QGIS Story!
 
 There&apos;s NO guarantees for anything! 
 Save files will likely not be compatible with newer test versions!
 
-Testing Comments (build 2016-08-04):
+Testing Comments (build 2016-08-10):
 - ALL active vector layers exported to destination folder as un-styled GeoJSON and included in story
 - Rasterlayers in jpg or png exported to destination folder! (TIFF not supported by Open Layers 3)
+- Only raster in EPSG:3857 supported
+- Something is weired in Windows...
 - WMS Layers also added as layers! (And depend on OL3 support)
 - Advance story by clicking on title.
 - Templates and styles are in plugin folder &quot;storysource&quot;. Can be edited and more added!
 - Quick changes to style in CSS tab
-- Only story and style changes (not template or style source) is saved to story file.
-- Use &quot;python -m SimpleHTTPServer&quot; from Story folder and http://localhost:8000 for support in Chrome/Edge/Safari
+- Only story and style changes (not template or style source) are saved to story file.
+- Included Web Server presenting story on port 8080
+- Use &quot;python -m SimpleHTTPServer&quot; from Story folder and http://localhost:8000 if needed
+- For Python 3 clients: &quot;python -m http.server&quot;
 
 On the TODO list:
-- Start Python Web Server automatically, or with option to view source
 - Generate start scripts for Linux/Win/Mac to start presentation in web server (require python on client)
 - More (and nicer) templates and styles
-- Probably a few bugs
+- Probably a few bugs to eliminate and other irritations
 
 Not on TODO list:
 - Style Vector Layer (OL3 not supporting SLD, yet)
